2ef48825133fd345220c21ea1126627a61775d07
W1nCode <102694967+wincodedev@users.noreply.github.com>
2023-04-22 18:20:35 +0300

    Initial commit

---
+++ Commands/Fun/Truth.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+module.exports = {
+  name: "truth",
+  alias: ["t"],
+  desc: "truth",
+  cool: 3,
+  react: "🙄",
+  category: "Fun",
+  start: async (Miku, m, { text, prefix }) => {
+    const shibam = await axios.get(
+      "https://dull-plum-panda-gear.cyclic.app/truth"
+    );
+
+    await Miku.sendMessage(m.from, {image: { url: botImage3 },caption: `*${shibam.data}*`,}, { quoted: m });
+  },
+};
